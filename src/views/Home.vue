6d2f92bed1ae3a1ea3ff6e1de1c8bd79461a3575
Mao Córdova <mcq@teamq.biz>
2021-06-15 23:22:16 -0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Autodeploy App"/>
+    <HelloWorld msg="Welcome to Your Vue.js Autodeploy"/>
   </div>
 </template>
 
