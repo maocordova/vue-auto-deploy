d6d78d0f21542ef6a544571fd1d3cd44f271aa4a
Mao Cordova <mcq@teamq.biz>
2021-06-18 15:37:22 -0500

    test: env variables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>Loaded from {{ this.url }} </h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -36,6 +37,9 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: {
+    url: process.env.VUE_APP_URL
   }
 }
 </script>
