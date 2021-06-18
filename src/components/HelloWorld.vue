9ac68e4c8653f8eaf93612ca82d17df33f41bb0a
Mao Cordova <mcq@teamq.biz>
2021-06-18 15:44:03 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h3>Loaded from {{ this.url }} </h3>
+    <h3>Loaded from {{ url }} </h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -38,8 +38,10 @@ export default {
   props: {
     msg: String
   },
-  data: {
-    url: process.env.VUE_APP_URL
+  data() {
+    return {
+      url: process.env.VUE_APP_URL
+    }    
   }
 }
 </script>
